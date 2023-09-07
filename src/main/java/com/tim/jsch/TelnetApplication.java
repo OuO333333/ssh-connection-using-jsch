7bd24ecf7fd205dc667b1e3bb1251fb8e58572f0
Tim <ck1000356@gmail.com>
2023-09-07 16:34:55 +0800

    message

--- src/main/java/com/tim/jsch/TelnetApplication.java
+++ src/main/java/com/tim/jsch/TelnetApplication.java
@@ -5,18 +5,18 @@ public class TelnetApplication {
 	public static void main(String[] args) {
 		com.tim.jsch.shell.Telnet telnet =  new com.tim.jsch.shell.Telnet("192.168.220.211", 23, "admin", "admin");
 
-		String str1 = telnet.sendCommand("s");
-		String str2 = telnet.sendCommand("h");
-		String str3 = telnet.sendCommand("o");
-		String str4 = telnet.sendCommand("w");
-		String str5 = telnet.sendCommand(" ");
-		String str6 = telnet.sendCommand("i");
-		String str7 = telnet.sendCommand("p");
-		String str8 = telnet.sendCommand("i");
-		String str9 = telnet.sendCommand("f");
-		String str10 = telnet.sendCommand("\r");
-		System.out.println("result6: \n" + str6 + "\nend result6");
-		System.out.println("result10: \n" + str10 + "\nend result10");
+		telnet.sendCommand("s");
+		telnet.sendCommand("h");
+		telnet.sendCommand("o");
+		telnet.sendCommand("w");
+		telnet.sendCommand(" ");
+		String str = telnet.sendCommand("i");
+		telnet.sendCommand("p");
+		telnet.sendCommand("i");
+		telnet.sendCommand("f");
+		String str1 = telnet.sendCommand("\r");
+		System.out.println("result6: \n" + str + "\nend result6");
+		System.out.println("result10: \n" + str1 + "\nend result10");
 		// String str11 = telnet.sendCommand("show\r");
 		// System.out.println("result11: \n" + str11 + "\nend result11");
 
